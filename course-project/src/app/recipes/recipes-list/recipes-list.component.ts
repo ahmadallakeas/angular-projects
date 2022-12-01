831c1718e01b8553c0b878f645ad4b3dc7eccf1c
Ahmad Al Lakeas <ahmadallakeas@gmail.com>
2022-12-01 12:13:32 +0200

    further modification of the project

--- course-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
-export class RecipesListComponent {
-
+export class RecipesListComponent implements OnInit {
+  recepies: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://www.allrecipes.com/thmb/WqWggh6NwG-r8PoeA3OfW908FUY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/21014-Good-old-Fashioned-Pancakes-mfs_001-1fa26bcdedc345f182537d95b6cf92d8.jpg'
+    ),
+  ];
+  constructor() {}
+  ngOnInit() {}
 }
